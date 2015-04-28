a0fc7c6f1b7f203f8b9cf42974eadc2f5b38c547
Paolo Betti <paolo@artifex.it>
2015-04-28 23:57:55 +0200

    uses $_SERVER["HTTP_X_CLIENT_IP"], if set, instead of $_SERVER["REMOTE_ADDR"]

--- cloudflare.php
+++ cloudflare.php
@@ -48,9 +48,10 @@ function cloudflare_init() {
 		if (strpos($_SERVER["REMOTE_ADDR"], ":") === FALSE) {
 	
 			$cf_ip_ranges = array("199.27.128.0/21","173.245.48.0/20","103.21.244.0/22","103.22.200.0/22","103.31.4.0/22","141.101.64.0/18","108.162.192.0/18","190.93.240.0/20","188.114.96.0/20","197.234.240.0/22","198.41.128.0/17","162.158.0.0/15","104.16.0.0/12","172.64.0.0/13");
+            $ipv4 = ($_SERVER["HTTP_X_CLIENT_IP"] ? $_SERVER["HTTP_X_CLIENT_IP"] : $_SERVER["REMOTE_ADDR"]);
 			// IPV4: Update the REMOTE_ADDR value if the current REMOTE_ADDR value is in the specified range.
 			foreach ($cf_ip_ranges as $range) {
-				if (ipv4_in_range($_SERVER["REMOTE_ADDR"], $range)) {
+				if (ipv4_in_range($ipv4, $range)) {
 					if ($_SERVER["HTTP_CF_CONNECTING_IP"]) {
 						$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 					}
